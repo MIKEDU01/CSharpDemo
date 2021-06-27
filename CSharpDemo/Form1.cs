7bfa59f6191fda687761e52dbda31ac42965af8a
MIKEDU01 <MikeDu91@outlook.com>
2021-06-27 20:51:54 +0800

    计算可以将点连接成直线的路径

--- CSharpDemo/Form1.cs
+++ CSharpDemo/Form1.cs
@@ -16,5 +16,150 @@ namespace CSharpDemo
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Point[] points = new Point[7];
+            points[0] = new Point(0, 0);
+            points[1] = new Point(10, 0);
+            points[2] = new Point(-10, 0);
+            points[3] = new Point(0, 10);
+            points[4] = new Point(0, -10);
+            points[5] = new Point(20, 0);
+            points[6] = new Point(30, 0);
+
+            int len = points.Length;
+
+            MvNode[] nodes = new MvNode[len];
+
+            for (int i = 0; i < len; i++)
+            {
+                nodes[i] = new MvNode() { Point = points[i], Index = i };
+            }
+
+            double distance = 0;
+            double angle = 0;
+
+            for (int i = 0; i < len - 1; i++)
+            {
+                for (int j = i + 1; j < len; j++)
+                {
+                    distance = nodes[i].Distance(nodes[j]);
+
+                    if (distance <= 10)
+                    {
+                        angle = nodes[i].Angle(nodes[j]);
+
+                        nodes[i].ConnectedNodes.Add(nodes[j], new MvNodesData(distance, angle));
+                        nodes[j].ConnectedNodes.Add(nodes[i], new MvNodesData(distance, angle + 180));
+                    }
+                }
+            }
+
+            List<MvPath> paths = new List<MvPath>();
+
+            for (int i = 0; i < len; i++)
+            {
+                if (nodes[i].ConnectedNodesNum < 1)
+                {
+                    continue;
+                }
+
+                nodes[i].Flag = true;
+
+                foreach (var obj in nodes[i].ConnectedNodes)
+                {
+                    obj.Key.Flag = true;
+
+                    if (GetPaths(nodes, nodes[i], obj.Key, out MvPath res))
+                    {
+                        res.Parent = null;
+                        paths.Add(res);
+                    }
+
+                    obj.Key.Flag = false;
+                }
+
+                nodes[i].Flag = false;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="nodes">全部节点数组</param>
+        /// <param name="nFirstIndex">第1个点索引</param>
+        /// <param name="nSecondIndex">第2个点索引</param>
+        /// <param name="paths"></param>
+        /// <returns></returns>
+        private bool GetPaths(MvNode[] nodes, MvNode mFirstNode, MvNode mSecondNode, out MvPath path)
+        {
+            int len = mSecondNode.ConnectedNodesNum;
+            path = new MvPath(mFirstNode, mSecondNode);
+
+            // 第2个点连接的节点数量若为1，则该点应该为第1个点；因此，返回
+            if (len <= 1)
+            {
+                return false;
+            }
+            else
+            {
+                // 第1个点到第2个点的角度
+                double angle12 = mFirstNode.ConnectedNodes[mSecondNode].Angle;
+                double angle23 = 0;
+
+                // 遍历第2个节点连接的全部节点，即可能是第3个点
+                foreach (var obj in mSecondNode.ConnectedNodes)
+                {
+                    // 该节点已经被占用；有可能是第2节点
+                    if (obj.Key.Flag)
+                    {
+                        continue;
+                    }
+
+                    angle23 = mSecondNode.ConnectedNodes[obj.Key].Angle;
+
+                    double intersectAngle = Math.Abs(angle12 - angle23);
+
+                    if (intersectAngle > 180)
+                    {
+                        intersectAngle = 360 - intersectAngle;
+                    }
+
+                    if (intersectAngle > 10)
+                    {
+                        continue;
+                    }
+
+                    // 该节点(第3节点)只有1个连接的节点，说明是第2节点；
+                    // 因此,这是符合条件的第3个点
+                    if (obj.Key.ConnectedNodesNum == 1)
+                    {
+                        // 这是第3个点                        
+                        path.Nodes.Add(obj.Key);
+                    }
+                    else
+                    {
+                        obj.Key.Flag = true;
+
+                        // 以第2个点为起点，第2个点的子节点为第2个点：获得新的路径
+                        if (GetPaths(nodes, mSecondNode, obj.Key, out MvPath res))
+                        {
+                            res.Parent = path;
+                            path.Paths.Add(res);
+                        }
+
+                        obj.Key.Flag = false;
+                    }
+                }
+            }
+
+            return path.NodeNum > 0 || path.PathNum > 0;
+        }
     }
 }
---
+++ CSharpDemo/MvNode.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace CSharpDemo
+{
+    class MvNode
+    {
+        public int Index { get; set; }
+
+        public Point Point { get; set; }
+
+        public Dictionary<MvNode, MvNodesData> ConnectedNodes { get; } = new Dictionary<MvNode, MvNodesData>();
+
+        /// <summary>
+        /// 状态 TRUE表示被使用
+        /// </summary>
+        public bool Flag { get; set; }
+
+        /// <summary>
+        /// 链接的节点数量
+        /// </summary>
+        public int ConnectedNodesNum => ConnectedNodes.Count;
+
+        public double Distance(MvNode node)
+        {
+            return Math.Sqrt(Math.Pow((this.Point.X - node.Point.X), 2) + Math.Pow((this.Point.Y - node.Point.Y), 2));
+        }
+
+        public double Angle(MvNode node)
+        {
+            return Math.Atan2(node.Point.Y - Point.Y, node.Point.X - Point.X) * 180 / Math.PI;
+        }
+
+        public override string ToString()
+        {
+            return $"Index={Index} Point={Point} ConnectedNodes={ConnectedNodes.Count}";
+        }
+    }
+
+    class MvNodesData
+    {
+        public double Distance { get; set; }
+
+        public double Angle { get; set; }
+
+        public MvNodesData(double distance, double angle)
+        {
+            Distance = distance;
+            Angle = angle;
+
+            if (Angle < 0)
+            {
+                Angle += 360;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Distance={Distance:F3} Angle={Angle}";
+        }
+    }
+
+    class MvPath
+    {
+        /// <summary>
+        /// 当前路径的第2个节点连接的节点数量
+        /// </summary>
+        public int NodeNum => Nodes.Count;
+
+        /// <summary>
+        /// 当前路径的第2个节点为起点的节点数量
+        /// </summary>
+        public int PathNum => Paths.Count;
+
+        /// <summary>
+        /// 当前路径的第1个节点
+        /// </summary>
+        public MvNode FirstNode { get; set; }
+
+        /// <summary>
+        /// 当前路径的第2个节点
+        /// </summary>
+        public MvNode SecondNode { get; set; }
+
+        /// <summary>
+        /// 当前路径连接的“节点”的数量(即：该节点是终点，没有连接其他节点)
+        /// 此时，该路径第1个点和第2个点与该集合内的点，可组成3个点的路径；
+        /// 路径数量即为集合中节点的个数
+        /// </summary>
+        /// <returns></returns>
+        public List<MvNode> Nodes { get; } = new List<MvNode>();
+
+        /// <summary>
+        /// 路径点1和点2为起始点，连接的路径数量；
+        /// 此时，点2为新的路径的起点；点2的子节点依次为新路径的第2个点
+        /// </summary>
+        /// <returns></returns>
+        public List<MvPath> Paths { get; } = new List<MvPath>();
+
+        /// <summary>
+        /// 父节点
+        /// </summary>
+        public MvPath Parent { get; set; } 
+
+        public string Key { get; }
+
+        /// <summary>
+        /// 路径
+        /// </summary>
+        /// <param name="node1">路径第1个点</param>
+        /// <param name="node2">路径第2个点</param>
+        public MvPath(MvNode node1, MvNode node2)
+        {
+            FirstNode = node1;
+            SecondNode = node2;
+            Key = node1.Index + " - " + node2.Index;
+        }
+
+        public override string ToString()
+        {
+            return $"FirstIndex={FirstNode.Index} SecondIndex={SecondNode.Index} NodeNum={NodeNum} PathNum={PathNum}";
+        }
+    }
+}
